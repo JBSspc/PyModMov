3156fb33dcdea2d82d2d8dda08407da947fbdb6c
JBSspc <77463414+JBSspc@users.noreply.github.com>
2022-04-20 07:49:56 -0500

    Works

--- Lorenz/LorenzPy.py
+++ Lorenz/LorenzPy.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 #from sympy.abc import x,y,z
 
+
 class LorenzAttractor:
 
     # Lorenz attractor equations
@@ -17,7 +18,8 @@ class LorenzAttractor:
         dotZ = x*y - beta*z         # z' = x*y-beta*z
         return dotX, dotY, dotZ
 
-    # using scipy.integrate (solve_ivp) 
+    # Using scipy.integrate (solve_ivp) ==================================================================================================
+
     def OdeLorenz(self,x0,y0,z0, tmax,n, sigma, beta, rho,WIDTH, HEIGHT, DPI): 
         nSol = solve_ivp(self.Lorenz, (0,tmax), (x0,y0,z0), args = (sigma, beta, rho), dense_output=True)
         t = np.linspace(0, tmax,n)
@@ -40,7 +42,7 @@ class LorenzAttractor:
         plt.savefig('myLorenz.png', dpi=DPI)
         plt.show()      
         
-    # Runge-Kutta 4th order
+    # Runge-Kutta 4th order =============================================================================================================
 
     def L4rk(self,x,y,z, sigma, beta, rho):
         dotX = sigma * (y - x)      # x' = sigma*(y-x)
@@ -48,33 +50,72 @@ class LorenzAttractor:
         dotZ = x*y - beta*z         # z' = x*y-beta*z
         return dotX, dotY, dotZ
   
-    def Rk4(self,h,N,sigma, beta, rho):
-        xs = np.empty((N+1,))
-        ys = np.empty((N+1,))
-        zs = np.empty((N+1,))
+    def Euler(self,h,N,sigma, beta, rho):
+        xe = np.empty((N+1,))
+        ye = np.empty((N+1,))
+        ze = np.empty((N+1,))
 
-        xs[0], ys[0], zs[0] = (0.1,0.1,0.1)
+        xe[0], ye[0], ze[0] = (0.1,0.1,0.1)
 
         for i in range(N):
-          x_dot, y_dot, z_dot = self.L4rk(xs[i], ys[i], zs[i],sigma, beta, rho)
-          xs[i+1] = xs[i] + (x_dot*h)
-          ys[i+1] = ys[i] + (y_dot*h)
-          zs[i+1] = zs[i] + (z_dot*h)
+          x_dot, y_dot, z_dot = self.L4rk(xe[i], ye[i], ze[i],sigma, beta, rho)
+          xe[i+1] = xe[i] + (x_dot*h)
+          ye[i+1] = ye[i] + (y_dot*h)
+          ze[i+1] = ze[i] + (z_dot*h)
 
         fig = plt.figure()
         ax = fig.gca(projection = '3d')
 
-        ax.plot(xs, ys, zs)
+        ax.plot(xe, ye, ze)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        ax.set_title('Lorenz Attractor (Euler)')
+        plt.savefig('LorenzEuler.png', dpi=100)
+        plt.show()
+        # 'https://www.youtube.com/watch?v=kAvJRF9BeiA'
+
+    # Euler Method ======================================================================================================================
+            
+    def RK4(self, N,h, sigma, beta,rho):
+        xrk4 = np.empty((N+1,)) # prealojamos
+        yrk4 = np.empty((N+1,))
+        zrk4 = np.empty((N+1,))
+
+        xrk4[0], yrk4[0], zrk4[0] = (0.1,0.1,0.1)
+
+        for i in range(N):
+          x_dot, y_dot, z_dot = self.L4rk(xrk4[i],  yrk4[i], zrk4[i],sigma, beta, rho)
+         
+          k11 = x_dot
+          k21 = x_dot + 0.5*h*k11
+          k31 = x_dot + 0.5*h*k21
+          k41 = x_dot +h*k31
+          xrk4[i+1] = xrk4[i] + (h/6)*(k11 + 2*k21 + 2*k31 + k41)
+          
+          k12 = y_dot
+          k22 = y_dot + 0.5*h*k12
+          k32 = y_dot + 0.5*h*k22
+          k42 = y_dot +h*k32
+          yrk4[i+1] = yrk4[i] + (h/6)*(k12 + 2*k22 + 2*k32 + k42)
+          
+          k13 = z_dot
+          k23 = z_dot + 0.5*h*k13
+          k33 = z_dot + 0.5*h*k23
+          k43 = z_dot +h*k33
+          zrk4[i+1] = zrk4[i] + (h/6)*(k13 + 2*k23 + 2*k33 + k43)
+
+        fig = plt.figure()
+        ax = fig.gca(projection = '3d')
+
+        ax.plot(xrk4,yrk4,zrk4)
         ax.set_xlabel('x')
         ax.set_ylabel('y')
         ax.set_zlabel('z')
         ax.set_title('Lorenz Attractor (RK4)')
         plt.savefig('LorenzRK4.png', dpi=100)
         plt.show()
-
-            
-    def EulerLorenz():
-        pass
+        
 
 
 
@@ -90,7 +131,8 @@ def main():
     la = LorenzAttractor()
 
     la.OdeLorenz(x0,y0,z0, tmax,n, sigma, beta, rho,WIDTH, HEIGHT, DPI)
-    la.Rk4(h,N,sigma, beta, rho)
+    la.Euler(h,N,sigma, beta, rho)
+    la.RK4(N,h, sigma, beta,rho)
     
 
 if __name__ == "__main__":
